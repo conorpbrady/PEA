a8a55e02c8b397dcdb329f6b2ed376d062a9d13e
Conor Brady <conorpbrady@gmail.com>
2024-03-22 18:25:14 -0400

    add basic views and templates

--- pea/aggregator/views.py
+++ pea/aggregator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+from .models import EventEntry
 
 # Create your views here.
+
+def events_view(request):
+    template_name = 'events.html'
+
+    return TemplateResponse(request, template_name, {
+        'events': EventEntry.objects.all()
+        })
--- pea/pea/urls.py
+++ pea/pea/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from aggregator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('events', views.events_view, name='events')
 ]

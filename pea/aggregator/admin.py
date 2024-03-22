0671c432a09f194cea2f8c5950a51e3cb88d8570
Conor Brady <conorpbrady@gmail.com>
2024-03-22 17:55:59 -0400

    create models and add to admin

--- pea/aggregator/admin.py
+++ pea/aggregator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(EventEntry)
+admin.site.register(XpathImporter)
--- pea/aggregator/models.py
+++ pea/aggregator/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class EventEntry(models.Model):
+
+    owner = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length = 255)
+    title = models.CharField(max_length = 255)
+    tagline = models.CharField(max_length = 255)
+    link = models.CharField(max_length = 255)
+    cost = models.DecimalField(max_digits = 9, default = 0.00, decimal_places = 2)
+
+
+class Importer(models.Model):
+    pass
+
+class XpathImporter(Importer):
+
+    events_elements_xpath = models.CharField(max_length = 255)
+    date_xpath = models.CharField(max_length = 255)
+    time_xpath = models.CharField(max_length = 255)
+    location_xpath = models.CharField(max_length = 255)
+    title_xpath = models.CharField(max_length = 255)
+    tagline_xpath = models.CharField(max_length = 255)
+    link_xpath = models.CharField(max_length = 255)
+    cost_xpath = models.CharField(max_length = 255)
